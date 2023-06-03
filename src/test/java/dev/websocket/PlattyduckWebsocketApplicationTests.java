d3b244f9f3cf1167da9dccf91dc55d679d626737
minsu <naman79@naver.com>
2023-06-03 15:36:21 +0900

    add controller
    add CoreController

---
+++ src/test/java/dev/websocket/PlattyduckWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlattyduckWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
